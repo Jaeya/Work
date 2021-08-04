96d2d92b1d91ac4bae6204720ef623646d635870
Jaeya <yi2228@naver.com>
2021-08-04 15:54:19 +0900

    08_04 오후수업

--- Python Class 08_04_Class2.py
+++ Python Class 08_04_Class2.py
@@ -17,3 +17,16 @@ p1 = Person()
 p2 = Person()
 p3 = Person()
 print("인스턴스 갯수:{0}".format(Person.num_person))
+
+# 동적으로 확장이 된다.
+Person.title = "New Title"
+print(p1.title)
+print(p2.title)
+print(Person.title)
+
+# 인스턴스에 추가
+p1.age = 30
+print(p1.age)
+print(p2.age)
+
+
