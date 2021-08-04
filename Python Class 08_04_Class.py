f34b7de435128aa1aded4ec4153144d8b7f220ee
Jaeya <yi2228@naver.com>
2021-08-04 11:28:26 +0900

    08_04 오전

---
+++ Python Class 08_04_Class.py
@@ -0,0 +1,13 @@
+# Python Class 08_04_Class.py
+
+# 클래스를 정의
+
+class Person:
+    def __init__(self):
+        self.name = "default name" # 초기화
+    def print(self):
+        print("My name is {0}".format(self.name))
+
+# 인스턴스생성
+p1 = Person()
+p1.print()
